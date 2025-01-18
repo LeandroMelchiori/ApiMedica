f386813c9f5d213a313e0f71b4bdb7ab23b9a7f4
Leandro Melchiori <107371569+LeandroMelchiori@users.noreply.github.com>
2025-01-18 17:11:24 -0300

    tratado de errores

--- src/main/java/med/vol/api/domain/medico/DatosRegistroMedico.java
+++ src/main/java/med/vol/api/domain/medico/DatosRegistroMedico.java
@@ -8,11 +8,18 @@ import jakarta.validation.constraints.Pattern;
 import med.vol.api.domain.direccion.DatosDireccion;
 
 public record DatosRegistroMedico(
-        @NotBlank String nombre,
-        @NotBlank @Email String email,
-        @NotBlank String telefono,
-        @NotBlank @Pattern(regexp = "\\d{4,9}") String documento,
-        @NotNull Especialidad especialidad,
-        @NotNull @Valid DatosDireccion direccion
+        @NotBlank
+        String nombre,
+        @NotBlank(message = "{email.obligatorio}")
+        @Email(message = "{email.invalido}")
+        String email,
+        @NotBlank
+        String telefono,
+        @NotBlank @Pattern(regexp = "\\d{4,9}")
+        String documento,
+        @NotNull
+        Especialidad especialidad,
+        @NotNull @Valid
+        DatosDireccion direccion
 ) {
 }
