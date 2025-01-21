9996984f013e983a05a38e91d0e2f5050cd6d06b
Leandro Melchiori <107371569+LeandroMelchiori@users.noreply.github.com>
2025-01-21 00:13:59 -0300

    fix medicoRepository

--- src/test/java/med/vol/api/domain/medico/MedicoRepositoryTest.java
+++ src/test/java/med/vol/api/domain/medico/MedicoRepositoryTest.java
@@ -11,6 +11,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.time.DayOfWeek;
